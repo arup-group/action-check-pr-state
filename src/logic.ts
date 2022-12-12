3421e230aaf1d4d0f574fcff13ee324e7e0e2058
Martin Cramp <martin.cramp@arup.com>
2022-12-12 13:59:08 +0000

    D3-441 Add ID of vs17.4 pool

--- src/logic.ts
+++ src/logic.ts
@@ -170,7 +170,7 @@ function disableLabel(pr: PullsGetResponseData): boolean {
 }
 
 async function checkInDevops(actionContext: ActionContext): Promise<boolean> {
-  const poolIds = ['12', '22', '24', '25', '27', '28', '29', '31', '33', '34', '36', '37', '39']
+  const poolIds = ['12', '22', '24', '25', '27', '28', '29', '31', '33', '34', '36', '37', '39', '40']
 
   const jobs = poolIds.map(async poolId => {
     const response = await fetch(
